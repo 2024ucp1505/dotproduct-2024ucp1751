4ef501b0aaace612f7b15655583c0d304e23cf02
Hitesh <2024ucp1426@mnit.ac.in>
2025-11-11 15:06:14 +0530

    First

---
+++ README.md
@@ -0,0 +1,1 @@
+This intends to calculate the dot product of two vectors.
---
+++ vec.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+n = int(input("Enter the number of elements in vec 1\n"))
+m = int(input("Enter the number of elements in vec 2\n"))
+list1 = []
+list2 = []
+print("Enter elements of vector 1 : \n")
+for i in range(0,n):
+	ele = int(input("Enter the element please-> "))
+	list1.append(ele)
+print("Enter elements of vector 2 : \n")
+for i in range(0,m):
+	ele = int(input("Enter the element please -> "))
+	list2.append(ele)
+vec1 = np.array(list1)
+vec2 = np.array(list2)
+
+vecDot = 0
+for i in range (0,n):
+	vecDot += (list1[i] * list2[j])
+print("Dot product : ",vecDot,"\n")
